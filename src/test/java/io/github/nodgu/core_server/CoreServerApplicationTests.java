5fa8371c096105400e2df38d7aafbae42e27a5e2
Won, J.I <wji0227@naver.com>
2025-07-14 16:49:34 +0900

    chore: First commit

---
+++ src/test/java/io/github/nodgu/core_server/CoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nodgu.core_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
